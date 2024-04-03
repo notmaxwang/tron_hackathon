4b349a1fcf9f401823afff75eb55047c8d024180
notmaxwang <maxwang3737@gmail.com>
2024-04-03 00:07:53 -0700

    Frontend Changes

--- services/keyfi-frontend/src/Page/Chat.tsx
+++ services/keyfi-frontend/src/Page/Chat.tsx
@@ -1,58 +1,18 @@
-import { useState } from "react";
-// import { KeyFiAIServiceClient} from '../protos/keyFiAI.client'
-// import { SinglePromptRequest } from '../protos/keyFiAI';
-import { GrpcWebFetchTransport } from '@protobuf-ts/grpcweb-transport';
-// import keyFiAIService from '../protos/keyFiAI_pb'
+import React, { useState } from 'react';
 
-const [textValues, setTextValues] = useState({
-  prompt: '',
-  response: ''
-})
-
-const handleUpdatePrompt = (e) => {
-  setTextValues({ 
-    ...textValues,
-    prompt: e.target.value
-  })
-}
-
-const handleButtonClick = async (e) => {
-  let transport = new GrpcWebFetchTransport({
-    baseUrl: "http://localhost:8080"
-  });
-  const client = new KeyFiAIServiceClient(transport);
-  const request = SinglePromptRequest.create({
-    prompt: textValues.prompt
-  })
-  const call = await client.singlePrompt(request);
-  let response = await call.response
-  let status = await call.status
-  console.log("status: " + status)
-  console.log(response);
-  setTextValues({ 
-    ...textValues,
-    response: response.response
-  })
-}
-
-const onFormSubmit = (e) => {
-  e.preventDefault();
-  console.log("refresh prevented");
-};
 
 
 export default function Chat() {
 
   return(
     <>
-      <form onSubmit={onFormSubmit}>
+      <form>
           <label>Prompt:</label> 
           <br/>
-          <input type="text" onChange={handleUpdatePrompt} />
+          <input type="text"/>
           <br/>
-          <button onClick={handleButtonClick}>Submit</button>
+          <button>Submit</button>
         </form>
-        <p>{textValues.response}</p>
     </>
   );
 }
