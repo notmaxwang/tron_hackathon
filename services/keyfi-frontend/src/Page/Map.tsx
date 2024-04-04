e69a624f08e1c47ba5b9afc5a000dd9854931496
notmaxwang <maxwang3737@gmail.com>
2024-04-03 21:00:22 -0700

    Updated Map

--- services/keyfi-frontend/src/Page/Map.tsx
+++ services/keyfi-frontend/src/Page/Map.tsx
@@ -1,65 +1,72 @@
 import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
 import { QueryServiceClient } from '../../protos/query/query.client';
 import { KeyValuePair, GetValuesRequest, GetValuesResponse } from '../../protos/query/query';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GrpcWebFetchTransport } from '@protobuf-ts/grpcweb-transport';
 import Mapbox from '../Component/MapBox';
 import './Map.css';
 
-export default function Map() {
+export default class Map extends React.Component {
 
-  let GOOGLE_MAP_API_KEY:string = '';
+  GOOGLE_MAP_API_KEY:any = '';
 
-  let listings = [{name: 'Ferry Building', position:{lat: 37.7955, lng: -122.3937,}},
+  listings = [{name: 'Ferry Building', position:{lat: 37.7955, lng: -122.3937,}},
     {name: 'Coit Tower', position:{lat: 37.8024, lng: -122.4058}}];
 
-  const listComponents:any = [];
-  listings.forEach((listing, idx) => {
-    listComponents.push(<MarkerF key={idx} position={listing.position} onClick={() => handleClick(listing.name)}/>);
-  });
+  listComponents:any = [];
+  listName:any = [];
 
-  const listName:any = [];
-  listings.forEach((listing, idx) => {
-    listName.push(<li key={idx}>{listing.name}</li>);
-  });
+  addToList() {
+    this.listings.forEach((listing, idx) => {
+      this.listComponents.push(<MarkerF key={idx} position={listing.position} onClick={() => handleClick(listing.name)}/>);
+      this.listName.push(<li key={idx}>{listing.name}</li>);
+    });
+  }
+  
+  
 
 
-  useEffect(() => {
-    async function getAPIKey() {
-      let transport = new GrpcWebFetchTransport({
-        baseUrl: "http://localhost:8080"
-      });
-      const client = new QueryServiceClient(transport);
-      const request = GetValuesRequest.create({
-        keys: ['GOOGLE_MAPS_KEY']
-      })
-      const call = await client.getValues(request);
-      let response = await call.response;
-      let status = await call.status;
-      console.log("status: " + status)
-      if(response.keyValuePairs){
-        GOOGLE_MAP_API_KEY = response.keyValuePairs[0].value;
-      }
+  componentWillMount() {
+    this.addToList();
+    this.getAPIKey();
+  }
+
+  async getAPIKey() {
+    let transport = new GrpcWebFetchTransport({
+      baseUrl: "http://localhost:8080"
+    });
+    const client = new QueryServiceClient(transport);
+    const request = GetValuesRequest.create({
+      keys: ['GOOGLE_MAPS_KEY']
+    })
+    const call = await client.getValues(request);
+    let response = await call.response;
+    let status = await call.status;
+    console.log("status: " + status)
+    if(response.keyValuePairs){
+      this.GOOGLE_MAP_API_KEY = response.keyValuePairs[0].value;
     }
-    getAPIKey();
-  }, [])
+  }
 
-  function handleClick(locationName:any) {
+  handleClick(locationName:any) {
     console.log(locationName);
-    console.log(GOOGLE_MAP_API_KEY);
+    console.log(this.GOOGLE_MAP_API_KEY);
   }
 
   
-  return (
-    <div>
-      <div className='topSection'>
-        <Mapbox GOOGLE_MAP_API_KEY={GOOGLE_MAP_API_KEY} listComponents={listComponents}/>
-        <div className='listing'>
-          <p className='listingTitle'>Listings</p>
-          <ul>{listName}</ul>
+  render(){
+    return (
+      <div>
+        <div className='topSection'>
+          <Mapbox GOOGLE_MAP_API_KEY={this.GOOGLE_MAP_API_KEY} listComponents={this.listComponents}/>
+          <div className='listing'>
+            <p className='listingTitle'>Listings</p>
+            <ul>{this.listName}</ul>
+          </div>
         </div>
+        
       </div>
-      
-    </div>
-  );
+    );
+  }
+  
 }
