ad227fe9ec526018dcf2e1aedefcb237d84538f1
kristina-lim <kristinahlim@gmail.com>
2024-04-10 00:05:51 -0700

    Remove icon

--- services/keyfi-frontend/src/Global/Header.tsx
+++ services/keyfi-frontend/src/Global/Header.tsx
@@ -1,12 +1,11 @@
 import './Header.css';
-import logo from '../assets/key.png';
 
 export default function Header() {
   return(
     <header>
       <nav className="navbar-container">
         <ul>
-          <li><a href="/"><img src={logo} alt="Website logo" className="logo-image" /></a></li>
+          {/* <li><a href="/"><img src={logo} alt="Website logo" className="logo-image" /></a></li> */}
           <li><a href="/">Home</a></li>
           <li><a href="/chat">AI Chatbot</a></li>
           <li><a href="/map">Map</a></li>
