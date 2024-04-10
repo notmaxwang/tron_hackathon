2a7787bcce244d3a8e5d93bbf1e99fba8df6d621
kristina-lim <kristinahlim@gmail.com>
2024-04-10 00:32:34 -0700

    Center homepage

--- services/keyfi-frontend/src/Page/Home.tsx
+++ services/keyfi-frontend/src/Page/Home.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return(
     <>
       <div className='home-page-container'>
-        <h1 className='home-title'>Real estate made <span className='gradient-text'>simple</span></h1>
-        <h2 className='home-description'>Buy, sell, tour virtually, get approved <br /> for loans: all powered by AI </h2>
+        <p className='home-title'>Real estate made <span className='gradient-text'>simple</span></p>
+        <p className='home-description'>Buy, sell, tour virtually, get approved <br /> for loans: all powered by AI </p>
         <input className='home-search' type='search' placeholder="Find your newest property" />
       </div>
     </>
