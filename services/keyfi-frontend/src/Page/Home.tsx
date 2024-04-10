5beb09434e79c8e3ae375d0ec179597f28052976
kristina-lim <kristinahlim@gmail.com>
2024-04-10 00:03:48 -0700

    Change Home & Header

--- services/keyfi-frontend/src/Page/Home.tsx
+++ services/keyfi-frontend/src/Page/Home.tsx
@@ -8,8 +8,8 @@ export default function Home() {
     <>
       <div className='home-page-container'>
         <h1 className='home-title'>Real estate made <span className='gradient-text'>simple</span></h1>
-        <h3 className='home-description'>Buy, sell, tour virtually, get approved <br /> for loans: all powered by AI </h3>
-        <input type='search' placeholder="Find your newest property" />
+        <h2 className='home-description'>Buy, sell, tour virtually, get approved <br /> for loans: all powered by AI </h2>
+        <input className='home-search' type='search' placeholder="Find your newest property" />
       </div>
     </>
   );
