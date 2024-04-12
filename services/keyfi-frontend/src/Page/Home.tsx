75b47e557287eb20e4d42c4d7250b06fb315be9a
kristina-lim <kristinahlim@gmail.com>
2024-04-11 23:58:14 -0700

    Add sparkle

--- services/keyfi-frontend/src/Page/Home.tsx
+++ services/keyfi-frontend/src/Page/Home.tsx
@@ -1,5 +1,5 @@
 import './Home.css'
-
+import Sparkle from '../../../keyfi-frontend/src/assets/sparkle.png'
 
 export default function Home() {
 
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <div className='home-page-container'>
         <p className='home-title'>Real estate made <span className='gradient-text'>simple</span></p>
-        <p className='home-description'>Buy, sell, tour virtually, get approved <br /> for loans: all powered by AI </p>
+        <p className='home-description'>Buy, sell, tour virtually, get approved <br /> for loans: all powered by AI <img src={Sparkle} alt="" className="sparkle" /></p>
         <input className='home-search' type='search' placeholder="Find your newest property" />
       </div>
     </>
