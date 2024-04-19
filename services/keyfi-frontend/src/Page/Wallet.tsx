aa5f6462174203777910914b4a2d4f067ae3b71c
notmaxwang <maxwang3737@gmail.com>
2024-04-19 16:29:41 -0700

    added wallet connect/disconnect and sign feature

--- services/keyfi-frontend/src/Page/Wallet.tsx
+++ services/keyfi-frontend/src/Page/Wallet.tsx
@@ -1,18 +1,20 @@
 import { useState, useMemo } from 'react';
-import type { WalletError } from '@tronweb3/tronwallet-abstract-adapter';
+import type { AdapterName, WalletError } from '@tronweb3/tronwallet-abstract-adapter';
 import { WalletDisconnectedError, WalletNotFoundError } from '@tronweb3/tronwallet-abstract-adapter';
 import { useWallet, WalletProvider } from '@tronweb3/tronwallet-adapter-react-hooks';
 import { WalletModalProvider } from '@tronweb3/tronwallet-adapter-react-ui';
-import toast from 'react-hot-toast';
+import toast, {Toaster} from 'react-hot-toast';
 import { tronWeb } from './tronweb';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Alert } from '@mui/material';
 import { TronLinkAdapter } from '@tronweb3/tronwallet-adapter-tronlink';
 import { WalletConnectAdapter } from '@tronweb3/tronwallet-adapter-walletconnect';
+import {
+    WalletActionButton,
+    WalletConnectButton,
+    WalletDisconnectButton,
+    WalletSelectButton,
+} from '@tronweb3/tronwallet-adapter-react-ui';
 import { Button } from '@tronweb3/tronwallet-adapter-react-ui';
-
-const rows = [
-    { name: 'Connect Button', reactUI: Button },
-];
 
 export default function Wallet() {
     function onError(e: WalletError) {
@@ -50,34 +52,36 @@ export default function Wallet() {
     return (
         <WalletProvider onError={onError} autoConnect={true} disableAutoConnectOnLoad={true} adapters={adapters}>
             <WalletModalProvider>
-                <UIComponent />
+                <UIComponent/>
                 <Profile />
                 <SignDemo />
             </WalletModalProvider>
+            <Toaster />
         </WalletProvider>
     );
 }
 
 function UIComponent() {
+    const rows = [
+        { name: 'Connect Button', reactUI: WalletConnectButton },
+        { name: 'Disconnect Button', reactUI: WalletDisconnectButton },
+        { name: 'Action Button', reactUI: WalletActionButton },
+        { name: 'Select Button', reactUI: WalletSelectButton },
+    ];
     return (
         <div>
-            <h2>UI Component</h2>
             <TableContainer style={{ overflow: 'visible' }} component="div">
                 <Table sx={{  }} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Component</TableCell>
-                            <TableCell align="left">React UI</TableCell>
+                            <TableCell>Wallet</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {rows.map((row) => (
                             <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                <TableCell component="th" scope="row">
-                                    {row.name}
-                                </TableCell>
-                                <TableCell align="left">
-                                    <row.reactUI></row.reactUI>
+                                <TableCell align='center'>
+                                    <row.reactUI>{row.name}</row.reactUI>
                                 </TableCell>
                             </TableRow>
                         ))}
@@ -110,49 +114,58 @@ function SignDemo() {
   const { signMessage, signTransaction, address } = useWallet();
   const [message, setMessage] = useState('');
   const [signedMessage, setSignedMessage] = useState('');
-  const receiver = 'TMDKznuDWaZwfZHcM61FVFstyYNmK6Njk1';
+  const receiver = 'TV3bpJQcY7DVQCNcEsKrDc1L4qEkSJvwrr';
   const [open, setOpen] = useState(false);
 
   async function onSignMessage() {
+    // var str = "helloworld"; 
+    // var HexStr = tronWeb.toHex(str);
+    // const test = await tronWeb.trx.sign(HexStr);
+    // console.log(test);
+    // const res = await tronWeb.trx.sign(message);
+    if(!message) {
+        return(toast.error('Please type in a message to sign.'));
+    }
       const res = await signMessage(message);
       setSignedMessage(res);
+      console.log('signedmessage', res);
   }
 
   async function onSignTransaction() {
       const transaction = await tronWeb.transactionBuilder.sendTrx(receiver, tronWeb.toSun(0.001), address);
 
       const signedTransaction = await signTransaction(transaction);
-      // const signedTransaction = await tronWeb.trx.sign(transaction);
+    //   const signedTransaction = await tronWeb.trx.sign(transaction);
        await tronWeb.trx.sendRawTransaction(signedTransaction);
       setOpen(true);
   }
   return (
       <div style={{ marginBottom: 200 }}>
-          <h2>Sign a message</h2>
-          <p style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', wordBreak: 'break-all' }}>
-              You can sign a message by click the button.
-          </p>
-          <Button style={{ marginRight: '20px' }} onClick={onSignMessage}>
-              SignMessage
-          </Button>
-          <TextField
-              size="small"
-              onChange={(e:any) => setMessage(e.target.value)}
-              placeholder="input message to signed"
-          ></TextField>
-          <p>Your sigedMessage is: {signedMessage}</p>
-          <h2>Sign a Transaction</h2>
-          <p style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', wordBreak: 'break-all' }}>
-              You can transfer 0.001 Trx to &nbsp;<i>{receiver}</i>&nbsp;by click the button.
-          </p>
-          <Button onClick={onSignTransaction}>Transfer</Button>
-          {open && (
-              <Alert onClose={() => setOpen(false)} severity="success" sx={{ width: '100%', marginTop: 1 }}>
-                  Success! You can confirm your transfer on{' '}
-                  <a target="_blank" rel="noreferrer" href={`https://nile.tronscan.org/#/address/${address}`}>
-                      Tron Scan
-                  </a>
-              </Alert>
+        <h2>Sign a message</h2>
+        <p style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', wordBreak: 'break-all' }}>
+            You can sign a message by click the button.
+        </p>
+        <TextField
+            size="small"
+            onChange={(e:any) => setMessage(e.target.value)}
+            placeholder="input message to signed"
+        ></TextField>
+        <Button style={{ marginRight: '20px', }} onClick={onSignMessage}>
+            SignMessage
+        </Button>
+        {signedMessage?<p>Your sigedMessage is: {signedMessage}</p>:<></>}
+        <h2>Sign a Transaction</h2>
+        <p style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', wordBreak: 'break-all', margin: '15px' }}>
+            You can transfer 0.001 Trx to &nbsp;<i>{receiver}</i>&nbsp;by click the button.
+        </p>
+        <Button onClick={onSignTransaction}>Transfer</Button>
+        {open && (
+            <Alert onClose={() => setOpen(false)} severity="success" sx={{ width: '100%', marginTop: 1 }}>
+                Success! You can confirm your transfer on{' '}
+                <a target="_blank" rel="noreferrer" href={`https://nile.tronscan.org/#/address/${address}`}>
+                    Tron Scan
+                </a>
+            </Alert>
           )}
       </div>
   );
