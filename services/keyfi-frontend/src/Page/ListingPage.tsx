940294b0b30dc0b749fc09e5dcb2e4599b2a9aab
notmaxwang <maxwang3737@gmail.com>
2024-04-30 23:20:39 -0700

    Listing Page changes

--- services/keyfi-frontend/src/Page/ListingPage.tsx
+++ services/keyfi-frontend/src/Page/ListingPage.tsx
@@ -1,4 +1,6 @@
-
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import  Listing  from '../Component/Listing';
 
 export default function ListingPage () {
 
